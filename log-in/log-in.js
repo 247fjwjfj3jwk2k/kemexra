c1510a50505c664e33abaed38e13eac45b8589c5
247fjwjfj3jwk2k <124291567+247fjwjfj3jwk2k@users.noreply.github.com>
2026-05-04 21:14:59 +0300

    Add files via upload

---
+++ log-in/log-in.js
@@ -0,0 +1,23 @@
+const loginForm = document.querySelector('#loginForm');
+loginForm.addEventListener('submit',function(event) {
+    event.preventDefault();
+
+    const inputEmail = document.querySelector('#email').value.trim();
+    const inputPassword = document.querySelector('#password').value.trim();
+    const savedEmail = localStorage.getItem('useremail');
+    const savedPassword = localStorage.getItem('password');
+
+    if (inputEmail !== savedEmail ){
+        alert("please enter a vaild email ");
+        return;
+    }
+      if (inputPassword !== savedPassword){
+        alert("password is wrong , please try again");
+        return;
+    }
+
+     alert("Login Successful! Welcome to KEMEXRA."); 
+     window.location.href = "index.html";
+
+});
+
