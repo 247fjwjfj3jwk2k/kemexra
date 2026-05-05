3166049953bf85e8a250539eec391427979d1eb9
247fjwjfj3jwk2k <124291567+247fjwjfj3jwk2k@users.noreply.github.com>
2026-05-05 15:47:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-//ده خاص بعكس الالوان
-function toggleTheme() {
-    document.documentElement.classList.toggle('light-mode');
-
-    const btn = document.querySelector('.theme-btn');
-    if (document.documentElement.classList.contains('light-mode')) {
-        btn.textContent = '🌙 Dark';
-        localStorage.setItem('theme', 'light');
-    } else {
-        btn.textContent = '☀️ Light';
-        localStorage.setItem('theme', 'dark');
-    }
-}
-
-// حفظ اللون بتاع الموقع سواء دارك او لايت خلال التصفح
-window.addEventListener('DOMContentLoaded', function () {
-    const saved = localStorage.getItem('theme');
-    if (saved === 'light') {
-        document.documentElement.classList.add('light-mode');
-        const btn = document.querySelector('.theme-btn');
-        if (btn) btn.textContent = '🌙 Dark';
-    }
-});
-
-
-
 const viewport = document.getElementById('viewport');
 const nextBtn = document.getElementById('nextBtn');
 const prevBtn = document.getElementById('prevBtn');
