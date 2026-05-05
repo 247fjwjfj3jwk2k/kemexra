be6903cda9bf028dbf036a71964485a65092fe4e
247fjwjfj3jwk2k <124291567+247fjwjfj3jwk2k@users.noreply.github.com>
2026-05-05 20:54:06 +0300

    Update contact.js

--- tourism/contact.js
+++ tourism/contact.js
@@ -1,15 +1,27 @@
+const contactForm = document.querySelector('.myform');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const errorElement = document.getElementById('error');
 
-document.querySelector('.myform').addEventListener('submit', function(event) {
-    const name = document.getElementById('name').value.trim();
-    const email = document.getElementById('email').value.trim();
+window.onload = function() {
+    nameInput.value = localStorage.getItem('cont_name') || "";
+    emailInput.value = localStorage.getItem('cont_email') || "";
+};
+
+contactForm.addEventListener('submit', function(event) {
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
     const subject = document.getElementById('subject').value.trim();
     const message = document.getElementById('message').value.trim();
-    const errorElement = document.getElementById('error');
+
     if (name === "" || email === "" || subject === "" || message === "") {
-        event.preventDefault(); 
-        errorElement.innerText = "Please fill in all the required fields!";
+        event.preventDefault();
+        errorElement.innerText = "Please fill in all fields!";
     } else {
+        localStorage.setItem('cont_name', name);
+        localStorage.setItem('cont_email', email);
+        
         errorElement.innerText = "";
-        alert("Sent successfully!");
+        alert("Sent and Saved successfully!");
     }
 });
