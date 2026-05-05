4e789c69922f6954c25900851944a2ddcc78a2d1
247fjwjfj3jwk2k <124291567+247fjwjfj3jwk2k@users.noreply.github.com>
2026-05-05 15:31:25 +0300

    Update contact.js

--- tourism/contact.js
+++ tourism/contact.js
@@ -1,25 +1,3 @@
-function toggleTheme() {
-    document.documentElement.classList.toggle('light-mode');
-
-    const btn = document.querySelector('.theme-btn');
-    if (document.documentElement.classList.contains('light-mode')) {
-        btn.textContent = '🌙 Dark';
-        localStorage.setItem('theme', 'light');
-    } else {
-        btn.textContent = '☀️ Light';
-        localStorage.setItem('theme', 'dark');
-    }
-}
-
-// حفظ اللون بتاع الموقع سواء دارك او لايت خلال التصفح
-window.addEventListener('DOMContentLoaded', function () {
-    const saved = localStorage.getItem('theme');
-    if (saved === 'light') {
-        document.documentElement.classList.add('light-mode');
-        const btn = document.querySelector('.theme-btn');
-        if (btn) btn.textContent = '🌙 Dark';
-    }
-});
 
 document.querySelector('.myform').addEventListener('submit', function(event) {
     const name = document.getElementById('name').value.trim();
