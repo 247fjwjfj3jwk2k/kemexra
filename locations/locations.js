b92720673e4ab6eec645fc2698b675d0b46dc832
247fjwjfj3jwk2k <124291567+247fjwjfj3jwk2k@users.noreply.github.com>
2026-05-05 15:27:55 +0300

    Update locations.js

--- locations/locations.js
+++ locations/locations.js
@@ -1,30 +1,3 @@
-
-//ده خاص بعكس الالوان
-function toggleTheme() {
-    document.documentElement.classList.toggle('light-mode');
-
-    const btn = document.querySelector('.theme-btn');
-    if (document.documentElement.classList.contains('light-mode')) {
-        btn.textContent = '🌙 Dark';
-        localStorage.setItem('theme', 'light');
-    } else {
-        btn.textContent = '☀️ Light';
-        localStorage.setItem('theme', 'dark');
-    }
-}
-
-// حفظ اللون بتاع الموقع سواء دارك او لايت خلال التصفح
-window.addEventListener('DOMContentLoaded', function () {
-    const saved = localStorage.getItem('theme');
-    if (saved === 'light') {
-        document.documentElement.classList.add('light-mode');
-        const btn = document.querySelector('.theme-btn');
-        if (btn) btn.textContent = '🌙 Dark';
-    }
-});
-
-
-
 const carousel = document.getElementById('carousel');
 const leftBtn = document.getElementById('scrollLeftBtn');
 const rightBtn = document.getElementById('scrollRightBtn');
